bfe32aac0ae9f8d1224ba77ed21667680693f936
shanayamalik <shanaya.malik@gmail.com>
2025-12-16 00:35:49 -0800

    delete demo/design pages, clean up routes, fix Get in Touch button

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,6 @@ import VolunteersPage from './pages/VolunteersPage';
 import VoiceInterviewPage from './pages/VoiceInterviewPage';
 import ProfileCreationPage from './pages/ProfileCreationPage';
 import ChatPage from './pages/ChatPage';
-import CardLayoutOptions from './pages/CardLayoutOptions';
-import ProfileDesignOptions2 from './pages/ProfileDesignOptions2';
-import MergeDecisionPage from './pages/MergeDecisionPage';
-import OptionCDemo from './pages/OptionCDemo';
-import CarrieLiveDemo from './pages/CarrieLiveDemo';
-import ProfileWithPreferencesDemo from './pages/ProfileWithPreferencesDemo';
-import AccessibilitySetupPage from './pages/AccessibilitySetupPage';
-import SchedulingCalendar from "./components/SchedulingCalendar";
-import ProfileMenuPreview from './pages/ProfileMenuPreview';
-import HelpCenterPreview from './pages/HelpCenterPreview';
 import './styles/global.css';
 
 function App() {
@@ -27,23 +17,6 @@ function App() {
       <div className="app">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/volunteers" element={<VolunteersPage />} />
-          {/*  SchedulingCalendar */}
-          <Route
-            path="/test-calendar"
-            element={
-              <SchedulingCalendar
-                volunteerId={999}
-                volunteerName="Test Volunteer"
-                availableSlots={[
-                  { date: "2025-11-25", time: "10:00 AM", duration: 30 },
-                  { date: "2025-11-25", time: "2:00 PM", duration: 30 },
-                  { date: "2025-11-26", time: "6:00 PM", duration: 30 },
-                ]}
-                onSchedule={(slot) => console.log("Scheduled slot:", slot)}
-              />
-            }
-          />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/recover-password" element={<ForgotPasswordPage />} />
@@ -53,15 +26,6 @@ function App() {
           <Route path="/interview" element={<VoiceInterviewPage />} />
           <Route path="/profile-creation" element={<ProfileCreationPage />} />
           <Route path="/chat/:volunteerId" element={<ChatPage />} />
-          <Route path="/card-layouts" element={<CardLayoutOptions />} />
-          <Route path="/profile-designs" element={<ProfileDesignOptions2 />} />
-          <Route path="/merge-decision" element={<MergeDecisionPage />} />
-          <Route path="/option-c-demo" element={<OptionCDemo />} />
-          <Route path="/carrie-demo" element={<CarrieLiveDemo />} />
-          <Route path="/preferences-demo" element={<ProfileWithPreferencesDemo />} />
-          <Route path="/accessibility-setup" element={<AccessibilitySetupPage />} />
-          <Route path="/profile-menu-preview" element={<ProfileMenuPreview />} />
-          <Route path="/help-center-preview" element={<HelpCenterPreview />} />
         </Routes>
       </div>
     </BrowserRouter>
